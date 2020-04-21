d0fac26b9ca7c453a5b3c1d02b91b7d03895c75e
mixulidio <mixulidio@gmail.com>
2020-04-21 17:21:46 -0300

    teste de remoção bk

--- src/test/java/br/ce/wcaquino/tasks/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/APITest.java
@@ -60,4 +60,25 @@ public class APITest {
             .log().all()
         ;
     }
+
+    @Test
+    public void deveRemoverTarefaComSucesso() {
+        // inserir
+        Integer id = RestAssured.given()
+            .body("{\"task\" : \"Teste via API\",\"dueDate\":\"2020-12-30\"}")
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/todo")
+        .then()
+            .statusCode(201)
+            .extract().path("id")    
+        ;
+        // remover
+        RestAssured.given()
+        .when()
+            .delete("/todo/" + id)
+        .then()
+            .statusCode(204)
+        ;
+    }
 }
